5be0787aee9172ff49a28af68a31141e65c86ed3
athix123 <athiz.mubaroz2@gmail.com>
2022-06-09 11:14:12 +0700

    edit yml pm2 sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,1 +1,7 @@
-sudo systemctl restart max-dev
+#!/bin/bash
+if ! type pm2 > /dev/null
+then
+    sudo npm install -g pm2 && pm2 start ./dist/main.js --name test-action
+else
+    pm2 restart test-action
+fi
