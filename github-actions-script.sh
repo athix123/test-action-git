039f89a087beb6bd3d632eefbaf5e33f41326c96
athix123 <athiz.mubaroz2@gmail.com>
2022-06-09 15:08:53 +0700

    work!

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-    sudo npm install -g pm2 && pm2 start ./dist/main.js --name test-action
+    sudo npm install -g pm2 && pm2 start ./dist/main.js --name work
 else
-    pm2 restart test-action
+    pm2 restart work
 fi
